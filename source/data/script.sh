dbbf416de093678994ade116bc85b67296327bc8
Daniel <50288378+DAguirreAg@users.noreply.github.com>
2023-06-04 16:52:50 +0200

    Update script.sh

--- source/data/script.sh
+++ source/data/script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Define files location
+FILES_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Activate virtual environment
-source /home/daniel/Documents/GitHub/IP-to-Geolocation/source/data/venv/bin/activate
+source $FILES_PATH/venv/bin/activate
 
 # Execute Python script
-python3 ${PWD}/"country_data_downloader.py"
-python3 ${PWD}/"city_data_downloader.py"
-#python3 ${PWD}/"country_data_uploader.py"
+python3 $FILES_PATH/"country_data_downloader.py"
+python3 $FILES_PATH/"city_data_downloader.py"
+#python3 $FILES_PATH/"country_data_uploader.py"
 
 # Deactivate the virtual environment
 deactivate
